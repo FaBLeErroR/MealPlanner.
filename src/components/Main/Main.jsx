f9e1d6c6557837f6f2e622dda394b0da805061ce
Данил Жеревчук <zherevcukd@gmail.com>
2024-04-23 10:53:21 +1000

    add-button

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,32 +3,32 @@ import toast from './img/toast.jpg'
 import pizza from './img/pitsa.jpg'
 import { Link } from 'react-router-dom'
 
-function WeaksList () {
-    const Weeks = ['Week 1', 'Week 2', 'Week 3', 'Week 3']
-    return (
-        <dl className="Weeks">
-            <li>{Weeks[0]}</li>
-            <li>{Weeks[1]}</li>
-            <li>{Weeks[2]}</li>
-            <li>{Weeks[3]}</li>
-        </dl>
-    )
-}
+// function WeaksList () {
+//     const Weeks = ['Week 1', 'Week 2', 'Week 3', 'Week 3']
+//     return (
+//         <dl className="Weeks">
+//             <li>{Weeks[0]}</li>
+//             <li>{Weeks[1]}</li>
+//             <li>{Weeks[2]}</li>
+//             <li>{Weeks[3]}</li>
+//         </dl>
+//     )
+// }
 
-function DaysList () {
-    const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    return (
-        <dl className="Days">
-            <li>{Days[0]}</li>
-            <li>{Days[1]}</li>
-            <li>{Days[2]}</li>
-            <li>{Days[3]}</li>
-            <li>{Days[4]}</li>
-            <li>{Days[5]}</li>
-            <li>{Days[6]}</li>
-        </dl>
-    )
-}
+// function DaysList () {
+//     const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+//     return (
+//         <dl className="Days">
+//             <li><Link className='daysLinks' to='/food/synday'>{Days[0]}</Link></li>
+//             <li><Link className='daysLinks' to='/food/monday'>{Days[0]}</Link></li>
+//             <li><Link className='daysLinks' to='/food/tuseday'>{Days[0]}</Link></li>
+//             <li><Link className='daysLinks' to='/food/wenesday'>{Days[0]}</Link></li>
+//             <li><Link className='daysLinks' to='/food/thursday'>{Days[0]}</Link></li>
+//             <li><Link className='daysLinks' to='/food/friday'>{Days[0]}</Link></li>
+//             <li><Link className='daysLinks' to='/food/saturday'>{Days[0]}</Link></li>
+//         </dl>
+//     )
+// }
 
 function DayFood () {
     var name = "Toast with banana flavor b l a b l a b l a b b l a"
@@ -59,8 +59,8 @@ export default function Main () {
         <main>
             <div className='Schedule'>
                 <h2 className='Text'>Current <b>personal</b> meal plan</h2>
-                <WeaksList />
-                <DaysList />
+                {/* <WeaksList />
+                <DaysList /> */}
                 <DayFood />
             </div>
         </main>
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,6 +1,7 @@
 import './Settings.css'
 import TagPicker from 'rsuite/TagPicker';
 import 'rsuite/TagPicker/styles/index.css';
+import { Button } from 'react-bootstrap';
 
 export default function Settings () {
     const Users = ['user1', 'user2'].map(
@@ -10,10 +11,13 @@ export default function Settings () {
         <main>
             <div className='Settings'>
                 <h1 className='Text'>Choose user</h1>
-                <select className="UserSelector">
-                    <option class="NameText">user1</option>
-                    <option class="NameText">user2</option>
-                </select>
+                <div>
+                    <select className="UserSelector">
+                        <option class="NameText">user1</option>
+                        <option class="NameText">user2</option>
+                    </select>
+                    <Button className="userButton" variant="outlined">Choose</Button>
+                </div>
             </div>
         </main>
     )
